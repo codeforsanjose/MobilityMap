4d9124d9e171805a69287b3512d0d54d03dc4fd9
Wilson Wang <wilsonohno@gmail.com>
2016-01-29 03:45:32 +0000

    register handlebar helper and paste javascript html regex replacer

--- src/sa_web/static/libs/handlebars-helpers.js
+++ src/sa_web/static/libs/handlebars-helpers.js
@@ -1,6 +1,25 @@
 /*globals Handlebars, moment, jQuery */
 
 (function($) {
+  // Allow a valid URL to be linked
+  Handlebars.registerHelper('linkToUrl', function(text) {    
+    replacedText = text;
+
+    //URLs starting with http://, https://, or ftp://
+    replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
+    replacedText = replacedText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
+
+    //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
+    replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
+    replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
+
+    //Change email addresses to mailto:: links.
+    //replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
+    //replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
+
+    return new Handlebars.SafeString(replacedText);
+  });
+
   // Get the current url
   Handlebars.registerHelper('windowLocation', function(place_id) {
     return window.location;
